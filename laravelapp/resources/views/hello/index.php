58eafb24e5a7466fe72cd88d991c910847170709
hirasatatsuya <omametan1229@gmail.com>
2022-05-01 22:22:26 +0900

    add p-165

--- laravelapp/resources/views/hello/index.php
+++ laravelapp/resources/views/hello/index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>Heelo/Index</title>
+    <title>Hello/Index</title>
     <style>
         body {font-size:16pt; color: #999; }
         h1 {font-size: 100pt; text-align: right; color:#eee; 
@@ -10,6 +10,7 @@
   </head>
   <body>
     <h1>Index</h1>
-    <p>This is a sample page with php-template.</p>
+    <p><?php echo $msg; ?></p>
+    <p>ID = <?php echo $id; ?></p>
   </body>
 </html>
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+// Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', [HelloController::class, 'index']);
+Route::post('hello', [HelloController::class, 'post']);
 // Route::get('hello', 'HelloController@index'); 誤
